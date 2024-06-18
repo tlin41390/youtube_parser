2694bdc8c670c90b524f407b5d969317a330fa20
Terence Lin <tlin41@dons.usfca.edu>
2024-06-17 23:56:45 -0700

    can write metadata to videos or playlists

--- youtube_parser.py
+++ youtube_parser.py
@@ -1,51 +1,69 @@
 from pytube import YouTube,Playlist,Caption
-import urllib.request
+import requests
 from pydub import AudioSegment
+from mutagen.id3 import ID3, TIT2, TPE1, TALB, TRCK,TYER, COMM, APIC, TSO2
+import ffmpeg
+import re
+
 import os
 
-def convert_to_mp3(link,output_path = '.'):
-    yt = YouTube(link)
+def handle_playlist(link):
+    playlist = Playlist(link)
+    for videos in playlist.videos:
+       convert_to_mp3(videos)
+        
+
+def convert_to_mp3(yt,output_path = '.'):
     stream = yt.streams.filter(only_audio=True).first()
-    old_file = stream.download(filename=f"{yt.title}.mp4", output_path = output_path)
-    mp3_file = yt.title+ '.mp3'
+    #remove special characters/symbols 
+    proccessed_title = re.sub('[^a-zA-Z0-9 \n\.]','',yt.title)
+    old_file = stream.download(filename=f"{proccessed_title}.mp4", output_path = output_path)
+    mp3_file = proccessed_title+ '.mp3'
     
     AudioSegment.from_file(old_file).export(mp3_file,format='mp3')
     os.remove(old_file)
+    
+    thumbnail = yt.thumbnail_url
+    thumbnail_data = requests.get(thumbnail).content
+    thumbnail_path = proccessed_title+'.jpg'
+    
+    with open(thumbnail_path, 'wb') as f:
+        f.write(thumbnail_data)
+    
+    #Writes metadata to mp3 file
+    audio = ID3(mp3_file)
+    audio.add(TIT2(encoding=3, text=yt.title))
+    audio.add(TPE1(encoding=3, text= yt.author))
+    audio.add(TALB(encoding=3, text = yt.title))
+    # audio.add(COMM(encoding=3, text=yt.link))
+    audio.add(TYER(eoncoding=3, text=str(yt.publish_date)))
+    audio.add(APIC(
+        encoding=3,
+        mime='image/jpeg',
+        type=3,
+        desc=u'Cover',
+        data=thumbnail_data
+        
+    ))
+    audio.save(v2_version=3)
+    os.remove(thumbnail_path)
+    
+
+    
 
 
 def main():
     try:
-        link = input("Enter a YouTube URL to download: ")
-        directory = input("What directory would you like to downolad to?: ")
-        
-        if not os.path.exists(directory):
-            os.makedirs(directory)
-        
-        yt = YouTube(link)
-        stream = yt.streams.filter(only_audio=True).first()
-        old_file = stream.download(filename=f"{yt.title}.mp4", output_path = directory)
-        mp3_file = yt.title+ '.mp3'
-        
-        AudioSegment.from_file(old_file).export(mp3_file,format='mp3')
-        os.remove(old_file)
-        print("video downloaded in mp3")
-
-        print(yt.publish_date)
-        print(yt.author)
-        print(link)
-        print(yt.thumbnail_url)
-        print(yt.metadata)
-        
-        
-
-        response = urllib.request.urlopen(yt.thumbnail_url)
-        image_data= response.read()
-
-        with open(f"{yt.title}.jpg", "wb") as img_file:
-            img_file.write(image_data)
-        print("Thumbnail downloaded successfully.")
-
-            
+        is_playlist = input("Is playlist(y or n)?: ")
+        if is_playlist == "y":
+            playlist = input("Enter a YouTube playlist to download: ")
+            handle_playlist(playlist)
+        else:
+            link = input("Enter a YouTube URL to download: ")
+            directory = input("What directory would you like to downolad to?: ")
+            yt = YouTube(link)
+            convert_to_mp3(yt,directory or ".")
+    
     except Exception as e:
         print("An error occured: ", str(e))
 
